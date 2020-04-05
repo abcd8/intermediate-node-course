7980e438c30d4aeb05192cbf1cf0870a8ee98b15
abcd8 <brianbui.n9@gmail.com>
2020-04-05 09:23:41 +0700

    read user by id

--- server.js
+++ server.js
@@ -15,14 +15,52 @@ app.use(bodyParser.json());
 app.listen(PORT, () => console.log(`Server is listening on port:${PORT}`))
 
 // CREATE
-app.post('/users', (req,res) => {
-  // User.create()
+app.post('/users', (req, res) => {
+  User.create({
+    name: req.body.newData.name,
+    email: req.body.newData.email,
+    password: req.body.newData.password,
+  }, (err, data) => {
+    if (err) {
+      res.json({
+        success: true,
+        message: err
+      });
+    } else if (!data) {
+      res.json({
+        success: false,
+        message: 'Not Found'
+      })
+    } else {
+      res.json({
+        success: true,
+        data
+      });
+    }
+  });
 })
 
 app.route('/users/:id')
 // READ
-.get((req,res)=>{
-  // User.findById()
+.get((req, res) => {
+  User.findById(req.params.id, (err, data) => {
+    if (err) {
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data) {
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  });
 })
 // UPDATE
 .put((req,res)=>{
